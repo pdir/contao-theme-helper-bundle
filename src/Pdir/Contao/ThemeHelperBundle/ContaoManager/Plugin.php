0e089980187d75de5c56c31dc315e06f2320b4b3
Mathias Arzberger <develop@pdir.de>
2017-12-05 18:27:29 +0100

    remove core

--- src/Pdir/Contao/ThemeHelperBundle/ContaoManager/Plugin.php
+++ src/Pdir/Contao/ThemeHelperBundle/ContaoManager/Plugin.php
@@ -15,9 +15,8 @@
  */
 namespace Pdir\Contao\ThemeHelperBundle\ContaoManager;
 
-use Contao\CoreBundle\ContaoCoreBundle;
-use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
 
 /**
