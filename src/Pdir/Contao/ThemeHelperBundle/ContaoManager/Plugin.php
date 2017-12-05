b290b2c4861685f779750d615aecaec0992a26d1
Mathias Arzberger <develop@pdir.de>
2017-12-05 18:41:22 +0100

    test manager bundle

--- src/Pdir/Contao/ThemeHelperBundle/ContaoManager/Plugin.php
+++ src/Pdir/Contao/ThemeHelperBundle/ContaoManager/Plugin.php
@@ -15,9 +15,10 @@
  */
 namespace Pdir\Contao\ThemeHelperBundle\ContaoManager;
 
-use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 
 /**
  * Plugin for the Contao Manager.
