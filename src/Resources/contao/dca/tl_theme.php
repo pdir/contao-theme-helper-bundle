a71886d91bfc727db37a6d53f0d54bdecc24cf07
Mathias Arzberger <develop@pdir.de>
2019-07-16 12:27:26 +0200

    add theme class in be

--- src/Resources/contao/dca/tl_theme.php
+++ src/Resources/contao/dca/tl_theme.php
@@ -68,7 +68,7 @@ class tl_theme_extended extends tl_theme
 		    {
 		        $themeUrl .= 'buy-' . $row['pdir_th_short_code'] . '.html';
             }
-			$label = '<span><a href="' . $themeUrl . '" target="_blank" rel="noopener"><img src="bundles/themehelper/img/buy_theme.png" ' .
+			$label = '<span class="contaoThemesNet"><a href="' . $themeUrl . '" target="_blank" rel="noopener"><img src="bundles/themehelper/img/buy_theme.png" ' .
                 ' title="' . $GLOBALS['TL_LANG']['tl_theme']['buyTheme'] . '"' .
 				' style="width:25px;margin: 10px 10px 10px 0;"></a></span>' . $label;
 		}
@@ -80,24 +80,26 @@ class tl_theme_extended extends tl_theme
 		if ($row['pdir_th_description'] != '')
 		{
 			// add info icon
-			$html = '<i class="icon" onmouseover="document.getElementById(\'themeDesc\').style.display = \'block\'"'.
+			$html = '<i class="icon contaoThemesNet" onmouseover="document.getElementById(\'themeDesc\').style.display = \'block\'"'.
 				' onmouseout="document.getElementById(\'themeDesc\').style.display = \'none\'"'.
 				' style="color:#fff;border-radius:50%;font-weight:bold;padding:3px;border:1px solid #649d9a;background:#649d9a;margin-left:10px;">i</i>';
 
 			// license status
+            $html .= '<span class="contaoThemesNet">';
 			if($row['pdir_th_license_domain'] != '')
 				$html .= $GLOBALS['TL_LANG']['tl_theme']['pdir_th_payed_license_text'] . $row['pdir_th_license_domain'] .'<br>';
 			else
 				$html .= $GLOBALS['TL_LANG']['tl_theme']['pdir_th_license_text'] . '<br>';
+            $html .= '</span>';
 
-            $html .= '<i class="icon"'.
+            $html .= '<i class="icon contaoThemesNet"'.
                 ' title="'.$GLOBALS['TL_LANG']['MSC']['checkDomainButtonTitle'].'"'.
                 ' style="font-style:normal;color:#fff;font-weight:bold;padding:5px;border:1px solid #649d9a;background:#649d9a;"'.
                 ' onclick="Backend.getScrollOffset();Backend.openModalSelector({\'width\':768,\'title\':\'' . $GLOBALS['TL_LANG']['MSC']['checkDomainButtonText'] . '\',\'url\':\'contao?do=thLicence&shortCode='.$themeShortCode.'&popup=1&theme='.$row['id'].'\',\'id\':\'checkDomain\'});return false"'.
                 '">'.$GLOBALS['TL_LANG']['MSC']['checkDomainButtonText'].'</i>';
 
 			// desc
-			$html .= '<div id="themeDesc" style="display:none;line-height:1.2em;margin-top:5px;">';
+			$html .= '<div id="themeDesc" class="contaoThemesNet" style="display:none;line-height:1.2em;margin-top:5px;">';
 			$html .= \StringUtil::decodeEntities($row['pdir_th_description']);
 			$html .= '</div>';
 			$label = $label . $html;
