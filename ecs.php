2bd5a7f65dc31d2ca9dc77234f94f5cc19ecaed3
Mathias Arzberger <arzberger@pdir.de>
2023-03-22 12:02:06 +0100

    update ecs

--- ecs.php
+++ ecs.php
@@ -9,14 +9,14 @@ use Symplify\EasyCodingStandard\ValueObject\Option;
 $date = date('Y');
 
 $GLOBALS['ecsHeader'] = <<<EOF
-theme helper bundle for Contao Open Source CMS
+Theme Helper Bundle for Contao Open Source CMS
 
-Copyright (C) $date pdir / digital agentur <develop@pdir.de>
+Copyright (C) $date pdir GmbH / pdir / digital agentur <develop@pdir.de>
 
 @package    pdir/contao-theme-helper-bundle
 @link       https://github.com/pdir/contao-theme-helper-bundle
 @license    LGPL-3.0+
-@author     pdir GmbH <develop@pdir.de>
+@author     Mathias Arzberger <develop@pdir.de>
 
 For the full copyright and license information, please view the LICENSE
 file that was distributed with this source code.
